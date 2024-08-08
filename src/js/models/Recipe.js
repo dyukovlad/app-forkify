cf5457d92ea1a9843c010818e9c7bfec31988a8d
Vladislav Dyukov <dyukovlad@gmail.com>
2024-08-08 11:11:08 +0300

    change api

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { key, proxy } from '../config';
+import { BASE_URL } from '../config';
 
 export default class Recipe {
   constructor(id) {
@@ -8,15 +8,13 @@ export default class Recipe {
 
   async getRecipe() {
     try {
-      const res = await axios(
-        `${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`
-      );
-      // https://food2fork.ca/api/recipe/search/?page=2&query=beef carrot potato onion
-      this.title = res.data.recipe.title;
-      this.author = res.data.recipe.publisher;
-      this.img = res.data.recipe.image_url;
-      this.url = res.data.recipe.source_url;
-      this.ingredients = res.data.recipe.ingredients;
+      const { data } = await axios(`${BASE_URL}get?rId=${this.id}`);
+
+      this.title = data.recipe.title;
+      this.author = data.recipe.publisher;
+      this.img = data.recipe.image_url;
+      this.url = data.recipe.source_url;
+      this.ingredients = data.recipe.ingredients;
     } catch (error) {
       console.log(error);
       alert('Something went wrong :(');
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
-import { key, proxy } from '../config';
+import { BASE_URL } from '../config';
 
-export default class Search{
-    
-     constructor(query){
-         this.query = query;
-     }
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
 
-    async getResults(){
-        try{
-            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;
-        } catch(error) {
-            console.log(error);
-        }
-    
+  async getResults() {
+    try {
+      const res = await axios(`${BASE_URL}search?q=${this.query}`);
+
+      this.result = res.data.recipes;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
