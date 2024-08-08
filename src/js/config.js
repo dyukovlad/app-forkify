1ffa69a9f84a3cd02cc42ed79f80b4530ccc6022
Vladislav Dyukov <dyukovlad@gmail.com>
2024-08-08 11:10:58 +0300

    del trash and comments

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const proxy = 'https://cors-anywhere.herokuapp.com/';
 export const key = '4e0f1f7c2a3eac969e6f86ff43ec9336';
 // export const key = '767de86f8ea74854742098dacb4e2271';
 // export const key = '462b1cc8d4f2730081462fbc65136320';
+// export const key = '9c8b06d329136da358c2d00e76946b0111ce2c48';
